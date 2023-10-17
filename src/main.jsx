48c6e15e18e5402403d5e24bbf65067ec2f51cd0
mehulrajdev <mehulrajpnbe@gmail.com>
2023-10-18 03:00:24 +0530

    updated aboutPage

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import About from './Component/About.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import About from "./Component/About.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-
-    <About/>
-  </React.StrictMode>,
-)
+    <About />
+  </React.StrictMode>
+);
