0bd3e63c9591afd25dfe23716607a5535ec1f792
mehulrajdev <mehulrajpnbe@gmail.com>
2023-10-17 15:49:44 +0530

    updated aboutPage

--- src/Component/About.jsx
+++ src/Component/About.jsx
@@ -1,15 +1,14 @@
 import React from "react";
 
 const About = () => {
+    let message ='The Professional Creative Team for Growth-up Works'
   return (
     <section className="section-white">
       <div className="container">
-        About
         <div className="row">
           <div className="col-md-12 text-center">
-            <h2 className="section-title">
-                The Team
-            </h2>
+            <h2 className="section-title">The Team Work</h2>
+            <p className="section-subtitle">{message}</p>
           </div>
         </div>
       </div>
