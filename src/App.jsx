eda7edec3065ce7fe25835f9a44b3cf74a42a8cf
mehulrajdev <mehulrajpnbe@gmail.com>
2023-10-18 14:07:46 +0530

    updated ContactPage

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+import React from "react";
 
-  return (
-    <>
-      <div className="flex items-center">
-        <a href="https://vitejs.dev" target="">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1 className='text-3xl font-bold underline'>Vite + React</h1>
-      <div className="card backdrop-brightness-75 my-12">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p className='text-[#50d71e]' >
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs text-[#000]">
-        Click on  Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+import "./App.css";
+import About from "./Component/About";
+import Contact from "./Component/Contact";
 
-export default App
+
+const App = () => (
+  <div className="App">
+  <About/>
+  <Contact/>
+  </div>
+);
+
+export default App;
