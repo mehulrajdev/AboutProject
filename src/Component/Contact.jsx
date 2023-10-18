413ebfb4eae976dff8d1f0cffe7c5d933585d436
mehulrajdev <mehulrajpnbe@gmail.com>
2023-10-18 14:12:49 +0530

    updated ContactPage

--- src/Component/Contact.jsx
+++ src/Component/Contact.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React from "react";
 
 const Contact = () => {
   return (
-    <div>Contact</div>
-  )
-}
+    <div className="h">
+      <div className="">
+        <div className="">Contact us</div>
+      </div>
+      <b className="">{`Get In `}</b>
+      <b className="">Touch</b>
+      <div className="">
+        We will contact again after receive your request in 24h
+      </div>
+      <div className="">
+        <div className=".divcommunCard}">
+          <div className=".divicon}">
+            <img className=".mail3dpngIcon1}" alt="" src="mail3d.png.png" />
+          </div>
+          <b className=".heading53}">contact@Iteck.com</b>
+        </div>
+        <div className=".divcommunCard1}">
+          <div className=".divicon1}">
+            <img className=".map3dpngIcon1}" alt="" src="map3d.png.png" />
+          </div>
+          <b className=".heading54}">58 Howard St, Toronto</b>
+        </div>
+        <div className=".divcommunCard2">
+          <div className=".divicon2}">
+            <img className=".msg3dpngIcon1}" alt="" src="msg3d.png.png" />
+          </div>
+          <b className=".heading55}">(+23) 5535 68 68</b>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Contact;
