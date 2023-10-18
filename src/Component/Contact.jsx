0f4ee7ab76c04d7eea33c45bf7628fe6a7bc9672
mehulrajdev <mehulrajpnbe@gmail.com>
2023-10-18 16:02:54 +0530

    updated ContactPage

--- src/Component/Contact.jsx
+++ src/Component/Contact.jsx
@@ -12,26 +12,26 @@ const Contact = () => {
       <div className="weWillContact1">
         We will contact again after receive your request in 24h
       </div>
-      {/* <div className="divcontent">
-        <div className=".divcommunCard}">
-          <div className=".divicon}">
-            <img className=".mail3dpngIcon1}" alt="" src="mail3d.png.png" />
+      <div className="divcontent">
+        <div className="divcommunCard">
+          <div className="divicon">
+            <img className="mail3dpngIcon1" alt="" src="mail3d.png.png" />
           </div>
-          <b className=".heading53}">contact@Iteck.com</b>
+          <b className="heading53}">contact@Iteck.com</b>
         </div>
-        <div className=".divcommunCard1}">
-          <div className=".divicon1}">
-            <img className=".map3dpngIcon1}" alt="" src="map3d.png.png" />
+        <div className="divcommunCard1}">
+          <div className="divicon1}">
+            <img className="map3dpngIcon1" alt="" src="map3d.png.png" />
           </div>
-          <b className=".heading54}">58 Howard St, Toronto</b>
+          <b className="heading54}">58 Howard St, Toronto</b>
         </div>
-        <div className=".divcommunCard2">
-          <div className=".divicon2}">
-            <img className=".msg3dpngIcon1}" alt="" src="msg3d.png.png" />
+        <div className="divcommunCard2">
+          <div className="divicon2}">
+            <img className="msg3dpngIcon1" alt="" src="msg3d.png.png" />
           </div>
-          <b className=".heading55}">(+23) 5535 68 68</b>
+          <b className="heading55">(+23) 5535 68 68</b>
         </div>
-      </div> */}
+      </div>
     </div>
   );
 };
