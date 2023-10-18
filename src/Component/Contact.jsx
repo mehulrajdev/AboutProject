68a6aba6e7e2ee5863f7e3b92400f94f2103924d
mehulrajdev <mehulrajpnbe@gmail.com>
2023-10-18 13:39:32 +0530

    updated ContactPage

--- src/Component/Contact.jsx
+++ src/Component/Contact.jsx
@@ -1,37 +1,9 @@
-import { FunctionComponent } from 'react';
-import styles from './MainSection.module.css';
+import React from 'react'
 
+const About = () => {
+  return (
+    <div>Contact</div>
+  )
+}
 
-const MainSection:FunctionComponent = () => {
-  	return (
-    		<div className={styles.mainSection1}>
-      			<div className={styles.small}>
-        				<div className={styles.contactUs}>Contact us</div>
-      			</div>
-      			<b className={styles.heading22}>{`Get In `}</b>
-      			<b className={styles.heading23}>Touch</b>
-      			<div className={styles.weWillContact1}>We will contact again after receive your request in 24h</div>
-      			<div className={styles.divcontent}>
-        				<div className={styles.divcommunCard}>
-          					<div className={styles.divicon}>
-            						<img className={styles.mail3dpngIcon1} alt="" src="mail3d.png.png" />
-          					</div>
-          					<b className={styles.heading53}>contact@Iteck.com</b>
-        				</div>
-        				<div className={styles.divcommunCard1}>
-          					<div className={styles.divicon1}>
-            						<img className={styles.map3dpngIcon1} alt="" src="map3d.png.png" />
-          					</div>
-          					<b className={styles.heading54}>58 Howard St, Toronto</b>
-        				</div>
-        				<div className={styles.divcommunCard2}>
-          					<div className={styles.divicon2}>
-            						<img className={styles.msg3dpngIcon1} alt="" src="msg3d.png.png" />
-          					</div>
-          					<b className={styles.heading55}>(+23) 5535 68 68</b>
-        				</div>
-      			</div>
-    		</div>);
-};
-
-export default MainSection;
+export default Contact
