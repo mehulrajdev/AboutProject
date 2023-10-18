9255a4716cbcee77b614e67a8579c5c3e13314a0
mehulrajdev <mehulrajpnbe@gmail.com>
2023-10-18 16:03:59 +0530

    updated ContactPage

--- src/Component/Contact.jsx
+++ src/Component/Contact.jsx
@@ -17,16 +17,16 @@ const Contact = () => {
           <div className="divicon">
             <img className="mail3dpngIcon1" alt="" src="mail3d.png.png" />
           </div>
-          <b className="heading53}">contact@Iteck.com</b>
+          <b className="heading53">contact@Iteck.com</b>
         </div>
-        <div className="divcommunCard1}">
-          <div className="divicon1}">
+        <div className="divcommunCard1">
+          <div className="divicon1">
             <img className="map3dpngIcon1" alt="" src="map3d.png.png" />
           </div>
-          <b className="heading54}">58 Howard St, Toronto</b>
+          <b className="heading54">58 Howard St, Toronto</b>
         </div>
         <div className="divcommunCard2">
-          <div className="divicon2}">
+          <div className="divicon2">
             <img className="msg3dpngIcon1" alt="" src="msg3d.png.png" />
           </div>
           <b className="heading55">(+23) 5535 68 68</b>
