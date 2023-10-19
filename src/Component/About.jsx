c9d946eb1f81763e68b8e060c459533b7d09ec50
mehulrajdev <mehulrajpnbe@gmail.com>
2023-10-19 15:36:33 +0530

    updated ContactPage

--- src/Component/About.jsx
+++ src/Component/About.jsx
@@ -38,60 +38,62 @@ const About = () => {
     //   </div>
     // </section>
     <div className="p-6">
-    <div className="mainSection1">
-      <div className="divcontainer1">
-        <div className="small">
-          <div className="theTeamWork1">The Team Work</div>
-        </div>
-        <b className="heading22">{`Our `}</b>
-        <b className="heading23">Leaders</b>
-        <div className="theProfessionalCreative1">
-          The Professional Creative Team for Growth-up Works
-        </div>
-        <div className="divteamCard">
-          <div className="linkHeading4">Michael Edwards</div>
-          <div className="smallCeo">CEO Founder</div>
-          <div className="divimg">
-            <img className="jpegIcon" alt="" src="1.png" />
+      <div className="mainSection1">
+        <div className="divcontainer1">
+          <div className="small">
+            <div className="theTeamWork1">The Team Work</div>
           </div>
-          <div className="social-icons">
-            <a href="#" className="me-1">
-              <i className="fab fa-twitter"></i>
-            </a>
-            <a href="#" className="me-1">
-              <i className="fab fa-facebook-f"></i>
-            </a>
-            <a href="#" className="me-1">
-              <i className="fab fa-linkedin-in"></i>
-            </a>
-            <a href="#">
-              <i className="fab fa-github"></i>
-            </a>
+          <b className="heading22">{`Our `}</b>
+          <b className="heading23">Leaders</b>
+          <div className="theProfessionalCreative1">
+            The Professional Creative Team for Growth-up Works
           </div>
-        </div>
-        <div className="divteamCard1">
-          <div className="linkHeading5">Bobby Kane</div>
-          <div className="smallCto">CTO</div>
-          <div className="divimg">
-            <img className="jpegIcon" alt="" src="2.png" />
-          </div>
-        </div>
-        <div className="divteamCard2">
-          <div className="linkHeading6">Robert Downey Jr</div>
-          <div className="smallProject1">Project Manager</div>
-          <div className="divimg">
-            <img className="jpegIcon" alt="" src="3.png" />
-          </div>
-        </div>
-        <div className="divteamCard3">
-          <div className="linkHeading7">Andrew Robertson</div>
-          <div className="smallMarketing1">Marketing Leader</div>
-          <div className="divimg">
-            <img className="jpegIcon" alt="" src="4.png" />
+          <div>
+            <div className="divteamCard">
+              <div className="linkHeading4">Michael Edwards</div>
+              <div className="smallCeo">CEO Founder</div>
+              <div className="divimg">
+                <img className="jpegIcon" alt="" src="1.png" />
+              </div>
+              <div className="social-icons">
+                <a href="#" className="me-1">
+                  <i className="fab fa-twitter"></i>
+                </a>
+                <a href="#" className="me-1">
+                  <i className="fab fa-facebook-f"></i>
+                </a>
+                <a href="#" className="me-1">
+                  <i className="fab fa-linkedin-in"></i>
+                </a>
+                <a href="#">
+                  <i className="fab fa-github"></i>
+                </a>
+              </div>
+            </div>
+            <div className="divteamCard1">
+              <div className="linkHeading5">Bobby Kane</div>
+              <div className="smallCto">CTO</div>
+              <div className="divimg">
+                <img className="jpegIcon" alt="" src="2.png" />
+              </div>
+            </div>
+            <div className="divteamCard2">
+              <div className="linkHeading6">Robert Downey Jr</div>
+              <div className="smallProject1">Project Manager</div>
+              <div className="divimg">
+                <img className="jpegIcon" alt="" src="3.png" />
+              </div>
+            </div>
+            <div className="divteamCard3">
+              <div className="linkHeading7">Andrew Robertson</div>
+              <div className="smallMarketing1">Marketing Leader</div>
+              <div className="divimg">
+                <img className="jpegIcon" alt="" src="4.png" />
+              </div>
+            </div>
           </div>
         </div>
       </div>
-    </div>
     </div>
   );
 };
