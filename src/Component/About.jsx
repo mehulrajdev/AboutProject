832003679d3d5029ef1c17fe5782e67193ee9156
mehulrajdev <mehulrajpnbe@gmail.com>
2023-10-19 15:14:05 +0530

    updated ContactPage

--- src/Component/About.jsx
+++ src/Component/About.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import styles from "./MainSection.module.css";
+import  from "./MainSection.module.css";
 import "./about.css";
 const About = () => {
   // let message ='The Professional Creative Team for Growth-up Works'
@@ -38,21 +38,21 @@ const About = () => {
     //     </div>
     //   </div>
     // </section>
-    <div className={styles.mainSection1}>
-      <div className={styles.divcontainer1}>
-        <div className={styles.small}>
-          <div className={styles.theTeamWork1}>The Team Work</div>
+    <div className="mainSection1">
+      <div className="divcontainer1">
+        <div className="small">
+          <div className={.theTeamWork1}>The Team Work</div>
         </div>
-        <b className={styles.heading22}>{`Our `}</b>
-        <b className={styles.heading23}>Leaders</b>
-        <div className={styles.theProfessionalCreative1}>
+        <b className={.heading22}>{`Our `}</b>
+        <b className={.heading23}>Leaders</b>
+        <div className={.theProfessionalCreative1}>
           The Professional Creative Team for Growth-up Works
         </div>
-        <div className={styles.divteamCard}>
-          <div className={styles.linkHeading4}>Michael Edwards</div>
-          <div className={styles.smallCeo}>CEO Founder</div>
-          <div className={styles.divimg}>
-            <img className={styles.jpegIcon} alt="" src="1.png" />
+        <div className={.divteamCard}>
+          <div className={.linkHeading4}>Michael Edwards</div>
+          <div className={.smallCeo}>CEO Founder</div>
+          <div className={.divimg}>
+            <img className={.jpegIcon} alt="" src="1.png" />
           </div>
           <div className="social-icons">
             <a href="#" className="me-1">
@@ -69,25 +69,25 @@ const About = () => {
             </a>
           </div>
         </div>
-        <div className={styles.divteamCard1}>
-          <div className={styles.linkHeading5}>Bobby Kane</div>
-          <div className={styles.smallCto}>CTO</div>
-          <div className={styles.divimg}>
-            <img className={styles.jpegIcon} alt="" src="2.png" />
+        <div className={.divteamCard1}>
+          <div className={.linkHeading5}>Bobby Kane</div>
+          <div className={.smallCto}>CTO</div>
+          <div className={.divimg}>
+            <img className={.jpegIcon} alt="" src="2.png" />
           </div>
         </div>
-        <div className={styles.divteamCard2}>
-          <div className={styles.linkHeading6}>Robert Downey Jr</div>
-          <div className={styles.smallProject1}>Project Manager</div>
-          <div className={styles.divimg}>
-            <img className={styles.jpegIcon} alt="" src="3.png" />
+        <div className={.divteamCard2}>
+          <div className={.linkHeading6}>Robert Downey Jr</div>
+          <div className={.smallProject1}>Project Manager</div>
+          <div className={.divimg}>
+            <img className={.jpegIcon} alt="" src="3.png" />
           </div>
         </div>
-        <div className={styles.divteamCard3}>
-          <div className={styles.linkHeading7}>Andrew Robertson</div>
-          <div className={styles.smallMarketing1}>Marketing Leader</div>
-          <div className={styles.divimg}>
-            <img className={styles.jpegIcon} alt="" src="4.png" />
+        <div className={.divteamCard3}>
+          <div className={.linkHeading7}>Andrew Robertson</div>
+          <div className={.smallMarketing1}>Marketing Leader</div>
+          <div className={.divimg}>
+            <img className={.jpegIcon} alt="" src="4.png" />
           </div>
         </div>
       </div>
