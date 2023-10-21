82c7eb7d095c383720e93b1619c2c7fc3b98ca96
mehulrajdev <mehulrajpnbe@gmail.com>
2023-10-22 02:58:53 +0530

    updated ContactPage

--- src/Component/About.jsx
+++ src/Component/About.jsx
@@ -12,7 +12,7 @@ const About = () => {
           <div className="col-md-12 text-center">
             <b className="text-[40px] leading-[52px]">Our </b>
             <b className="text-[40px] leading-[52px] leader">Leaders</b>
-            <p className="leading-6 text-[#212529]">{message}</p>
+            <p className="leading-[] text-[#212529]">{message}</p>
           </div>
 
           <div>
