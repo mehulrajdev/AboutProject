2465d7275517e16e16e0294e729a260d79dd5ce1
mehulrajdev <mehulrajpnbe@gmail.com>
2023-10-24 01:07:52 +0530

    updated ContactPage

--- src/Component/About.jsx
+++ src/Component/About.jsx
@@ -96,7 +96,7 @@ const About = () => {
                   <h6 className="text-[#6640f4] text-xl font-semibold">
                   Bobby Kane
                   </h6>
-                  <small className="text-[#999]">CEO Founder</small>
+                  <small className="text-[#999]">CEO</small>
                 </div>
               </div>
             </div>
