011d001e400611c94a36f783fa8ff9bd5964af44
mehulrajdev <mehulrajpnbe@gmail.com>
2023-10-24 01:12:22 +0530

    updated ContactPage

--- src/Component/About.jsx
+++ src/Component/About.jsx
@@ -178,7 +178,7 @@ const About = () => {
                 </div>
                 <div className="p-4">
                   <h6 className="text-[#6640f4] text-xl font-semibold">
-                  Robert Downeye
+                  Andrew Robertson
                   </h6>
                   <small className="text-[#999]">Project Manager</small>
                 </div>
