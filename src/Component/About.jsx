012459a470081571647f4f0ca6453c538c379b4f
mehulrajdev <mehulrajpnbe@gmail.com>
2023-10-23 15:47:45 +0530

    updated aboutPage

--- src/Component/About.jsx
+++ src/Component/About.jsx
@@ -148,7 +148,7 @@ const About = () => {
                   <h6 className="text-[#6640f4] text-xl font-semibold">
                     Andrew Robertson
                   </h6>
-                  <small className="text-[#999]">Marketing </small>
+                  <small className="text-[#999]">Marketing Lea </small>
                 </div>
               </div>
             </div>
