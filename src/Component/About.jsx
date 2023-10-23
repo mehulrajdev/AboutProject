705a42038cfb93b76c226e1db961336136b550e3
mehulrajdev <mehulrajpnbe@gmail.com>
2023-10-23 15:31:13 +0530

    updated aboutPage

--- src/Component/About.jsx
+++ src/Component/About.jsx
@@ -88,7 +88,7 @@ const About = () => {
               <div className="team-item mb-32 mb-lg-0 ">
                 <div className="h-[320px] rounded-2xl overflow-hidden relative">
                   <img
-                    src=".png"
+                    src="3.png"
                     className="w-full object-cover h-full"
                     alt="pic"
                   />
