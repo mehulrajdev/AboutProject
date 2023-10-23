6e7dc68eafee9dee425b672af0cc63e2925f9f4f
mehulrajdev <mehulrajpnbe@gmail.com>
2023-10-23 15:24:14 +0530

    updated aboutPage

--- src/Component/About.jsx
+++ src/Component/About.jsx
@@ -44,7 +44,7 @@ const About = () => {
                 </div>
                 <div className="p-4">
                   <h6 className="text-[#6640f4] text-xl font-semibold">
-                    
+                    Michael
                   </h6>
                   <small className="text-[#999]">CEO Founder</small>
                 </div>
