9af933c8b1932eeaa8cdd06dec27605865c25241
mehulrajdev <mehulrajpnbe@gmail.com>
2023-10-24 15:11:57 +0530

    updated ContactPage

--- src/Component/Contact.jsx
+++ src/Component/Contact.jsx
@@ -128,7 +128,11 @@ const Contact = () => {
                         ></textarea>
                       </div>
                     </div>
-                    <div className="col-lg-12 text-center"></div>
+                    <div className="col-lg-12 text-center">
+                      <div className="inline-flex mt-3 mb-3 form-check">
+                      <input type="checkbox" id="flexCheckDefault" class="form-check-input me-2 mt-0">
+                      </div>
+                    </div>
                     <div className="col-lg-12 text-center"></div>
                   </div>
                 </form>
