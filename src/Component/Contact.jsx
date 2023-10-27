2c439215c2e2fa2f10d3c8308ed8ce3339067faa
mehulrajdev <mehulrajpnbe@gmail.com>
2023-10-27 15:32:49 +0530

    updated ContactPage

--- src/Component/Contact.jsx
+++ src/Component/Contact.jsx
@@ -6,8 +6,8 @@ const Contact = () => {
     <div className="container ">
       <section className="section-padding ">
         <div className="container">
-          <div className="text-center mb-8 contact">
-            <small className="text-xs bg-[#f7f4ff] text-[#5842bc] mb-5 px-2.5 py-[5px] rounded-[5px]">
+          <div className="contact">
+            <small className=" xs text-xs bg-[#f7f4ff] text-[#5842bc] mb-5 px-2.5 py-[5px] rounded-[5px]">
               Contact us
             </small>
             <h2 className="m-10 text-4xl text-[#000]  ">
