e7123c3b88553f6647594ed3577564d9a04851cf
mehulrajdev <mehulrajpnbe@gmail.com>
2023-10-27 15:58:30 +0530

    updated ContactPage

--- src/Component/Contact.jsx
+++ src/Component/Contact.jsx
@@ -19,7 +19,7 @@ const Contact = () => {
           </div>
           <div className=" showall rounded-pill">
             <div className="show">
-              <div className=" mm">
+              <div className=" mmm">
                 <img src="mail.png"></img>
               </div>
               <div className="text">
@@ -29,7 +29,7 @@ const Contact = () => {
               </div>
             </div>
             <div className="show">
-              <div className="mm">
+              <div className="mmm">
                 <img src="map.png"></img>
               </div>
               <div className="text">
@@ -39,7 +39,7 @@ const Contact = () => {
               </div>
             </div>
             <div className="show">
-              <div className="mm">
+              <div className="mmm">
                 <img src="msg.png"></img>
               </div>
               <div className="text">
